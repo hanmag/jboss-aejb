18893f8f26198250e0e8b6555f865a8e77877096
Jason <wangjue1199@gmail.com>
2012-03-16 20:12:27 +0800

    add trigger interface

---
+++ aejb-api/src/main/java/javax/aejb/TransactionTrigger.java
@@ -0,0 +1,17 @@
+package javax.aejb;
+
+/**
+ * @author <a href="mailto:wangjue1199@gmail.com">Jason</a>
+ */
+public interface TransactionTrigger {
+
+	/**
+	 * Trigger a given event. If the event corresponds with one of the
+	 * exit-events of the state the current state is updated. Otherwise, nothing
+	 * happens.
+	 * 
+	 * @param eventName
+	 *            The name of the event being triggered
+	 */
+	void trigger(String objectId, String eventName);
+}
