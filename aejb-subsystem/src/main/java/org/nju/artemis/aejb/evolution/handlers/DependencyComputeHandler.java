e5a8370e1469f664cc42c848b674d4c6e67e9e5b
Jason <wangjue1199@gmail.com>
2012-03-19 19:31:43 +0800

    RuntimeDependencies

--- aejb-subsystem/src/main/java/org/nju/artemis/aejb/evolution/handlers/DependencyComputeHandler.java
+++ aejb-subsystem/src/main/java/org/nju/artemis/aejb/evolution/handlers/DependencyComputeHandler.java
@@ -2,32 +2,42 @@ package org.nju.artemis.aejb.evolution.handlers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import org.jboss.logging.Logger;
+import javax.aejb.AEjb;
 import org.nju.artemis.aejb.component.AEjbUtilities;
 import org.nju.artemis.aejb.component.AcContainer;
 import org.nju.artemis.aejb.evolution.OperationContext;
 import org.nju.artemis.aejb.evolution.OperationFailedException;
 
 /**
+ * This handler used to compute component's dependencies declared by {@link AEjb @AEjb}.
+ * 
  * @author <a href="wangjue1199@gmail.com">Jason</a>
  */
 public class DependencyComputeHandler implements OperationStepHandler {
+	Logger log = Logger.getLogger(DependencyComputeHandler.class);
 	private static final String HANDLER_NAME = "DependencyComputeHandler";
 	
 	@Override
 	public OperationResult execute(OperationContext context) throws OperationFailedException {
-		AEjbUtilities utilities = (AEjbUtilities) context.getContextData().get(AEjbUtilities.class);
-		String targetName = context.getTargetName();
+		log.info("------- DependencyComputeHandler Start -------");
+		final AEjbUtilities utilities = (AEjbUtilities) context.getContextData().get(AEjbUtilities.class);
+		final String targetName = context.getTargetName();
 		if(utilities == null || targetName == null)
 			throw new OperationFailedException("Don't have enough resources to compute neighbors.");
 		List<String> neighbors = new ArrayList<String>();
 		for(AcContainer container:utilities.getAllContainers()) {
-			List<String> dependencies = container.getDependencies();
+			Set<String> dependencies = container.getRuntimeDependencies();
+			log.info("container = " + container.getAEJBName() + ", dependencies = " + dependencies);
 			if(dependencies != null && dependencies.contains(targetName)) {
 				neighbors.add(container.getAEJBName());
 			}
 		}
+		log.info(targetName + "'s neighbors = " + neighbors);
 		context.getContextData().put("neighbors", neighbors);
+		log.info("------- DependencyComputeHandler Stop -------");
 		return OperationResult.Expected;
 	}
 
